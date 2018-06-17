fdce9dea51a111e0154e3c0901dafbbbfb24eaf6
Sapna Deraje Radhakrishna <sapna.dr@gmail.com>
2018-06-17 13:06:59 -0700

    Webflux Controller Advice

--- src/main/java/org/maxwell/controllers/ControllerExceptionHandler.java
+++ src/main/java/org/maxwell/controllers/ControllerExceptionHandler.java
@@ -1,26 +1,28 @@
 package org.maxwell.controllers;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.support.WebExchangeBindException;
+import org.thymeleaf.exceptions.TemplateInputException;
 
 import lombok.extern.slf4j.Slf4j;
-
 
 @Slf4j
 @ControllerAdvice
 public class ControllerExceptionHandler {
 
-    /*@ResponseStatus(HttpStatus.BAD_REQUEST)
-    @ExceptionHandler(NumberFormatException.class)
-    public ModelAndView handleNumberFormat(Exception exception){
+	@ResponseStatus(HttpStatus.BAD_REQUEST)
+	@ExceptionHandler({ WebExchangeBindException.class, TemplateInputException.class })
+	public String handleBindingException(Exception exception, Model model) {
 
-        log.error("Handling Number Format Exception");
-        log.error(exception.getMessage());
+		log.error("Handling Binding Exception");
+		log.error(exception.getMessage());
 
-        ModelAndView modelAndView = new ModelAndView();
+		model.addAttribute("exception", exception);
 
-        modelAndView.setViewName("400error");
-        modelAndView.addObject("exception", exception);
-
-        return modelAndView;
-    }*/
+		return "400error";
+	}
 }
--- src/main/java/org/maxwell/controllers/RecipeController.java
+++ src/main/java/org/maxwell/controllers/RecipeController.java
@@ -1,16 +1,20 @@
 package org.maxwell.controllers;
 
 import org.maxwell.commands.RecipeCommand;
+import org.maxwell.exceptions.NotFoundException;
 import org.maxwell.services.RecipeService;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -71,15 +75,13 @@ public class RecipeController {
 		return "redirect:/";
 	}
 
-	/*
-	 * @ResponseStatus(HttpStatus.NOT_FOUND)
-	 * 
-	 * @ExceptionHandler(NotFoundException.class) public ModelAndView
-	 * handleNotFound(Exception exception) {
-	 * log.error("Handling not found exception"); log.error(exception.getMessage());
-	 * ModelAndView modelAndView = new ModelAndView();
-	 * modelAndView.setViewName("404error"); modelAndView.addObject("exception",
-	 * exception); return modelAndView; }
-	 */
+	@ResponseStatus(HttpStatus.NOT_FOUND)
+	@ExceptionHandler(NotFoundException.class)
+	public String handleNotFound(Exception exception, Model model) {
+		log.error("Handling not found exception");
+		log.error(exception.getMessage());
+		model.addAttribute("exception", exception);
+		return "404error";
+	}
 
 }
